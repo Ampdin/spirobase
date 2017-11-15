aed74799c6b63de8e4ffbeaf58f88255858c83e2
spirovanni <x@ampdin.com>
2017-11-14 22:06:22 -0800

    Catch up
    Yes sir!

--- armory/src/main/java/com/spirovanni/blackshields/domain/enumeration/DepartmentMajor.java
+++
@@ -1,8 +0,0 @@
-package com.spirovanni.blackshields.domain.enumeration;
-
-/**
- * The DepartmentMajor enumeration.
- */
-public enum DepartmentMajor {
-    APPS, BEAUTY, BOOKS, MUSIC, GROCERY, FINANCIAL, HEALTH, OFFICE, SOFTWARE
-}
